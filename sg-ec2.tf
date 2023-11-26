d49ee897f1181d17f7f9703fd39e1f2afc2f0414
dgprofile <90986919+dgprofile@users.noreply.github.com>
2023-11-27 00:12:17 +0530

    msg1

--- sg-ec2.tf
+++ sg-ec2.tf
@@ -22,7 +22,7 @@ resource "aws_security_group" "t-sg" {
     }
 
   egress {
-    from_port        = 0
+    from_port        = 00
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
